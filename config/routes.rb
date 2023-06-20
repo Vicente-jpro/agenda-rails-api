854aa0bbf92ea81b3fa969bc607439533b5250a2
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-20 17:46:26 +0100

    scope added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :contacts
+
+
+  scope '/api' do
+    root "contacts#index"
+    resources :contacts
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
