ae4484553bd42e74bef8aa6646b968279ed66bc6
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-20 22:31:17 +0100

    Bug fixed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -46,6 +46,11 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:name, :emails, :birthdate, :kind_id, phones_attributes: [:number])
+      params.require(:contact).permit(
+        :name, 
+        :emails, 
+        :birthdate, 
+        :kind_id, 
+        phones_attributes: [:id, :number, :_destroy])
     end
 end
